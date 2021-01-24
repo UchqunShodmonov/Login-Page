ff8036a6a05d0018f2cb3665a89309b104d99f09
uchkun <shodmonov.uchqun@gmail.com>
2021-01-24 22:36:40 +0500

    init commit

---
+++ app/src/main/java/uz/uchqun/login_sample/core/model/LoginResponse.kt
@@ -0,0 +1,5 @@
+package uz.uchqun.login_sample.core.model
+
+data class LoginResponse(
+    val user: User
+)
---
+++ app/src/main/java/uz/uchqun/login_sample/ui/MainActivity.kt
@@ -0,0 +1,28 @@
+package uz.uchqun.login_sample.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.asLiveData
+import uz.uchqun.login_sample.R
+import uz.uchqun.login_sample.core.data.UserPreferences
+import uz.uchqun.login_sample.core.util.startNewActivity
+import uz.uchqun.login_sample.ui.auth.AuthActivity
+import uz.uchqun.login_sample.ui.home.HomeActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        val userPreferences = UserPreferences(this)
+        userPreferences.authToken.asLiveData().observe(this, Observer {
+            val activity = if (it == null) AuthActivity::class.java else HomeActivity::class.java
+            startNewActivity(activity)
+        })
+
+
+    }
+}
